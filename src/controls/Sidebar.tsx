868997cb703996a1359f5d4e53d6709b9a927021
Jussi Soini <jjsoini@gmail.com>
2023-11-02 04:39:02 +0200

    removed dicesetpicker

--- src/controls/Sidebar.tsx
+++ src/controls/Sidebar.tsx
@@ -23,8 +23,6 @@ export function Sidebar() {
       }}
     >
       <Stack p={1} gap={1} alignItems="center">
-        <DiceSetPicker />
-        <Divider flexItem sx={{ mx: 1 }} />
         <DicePicker />
         <Divider flexItem sx={{ mx: 1 }} />
         <DiceHidden />
